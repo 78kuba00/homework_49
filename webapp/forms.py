639c1bbdf6a6e50f47de9672bdfdd0955505a4f4
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-11-22 18:49:44 +0600

    fixed form by the TZ

--- webapp/forms.py
+++ webapp/forms.py
@@ -4,7 +4,7 @@ from webapp.models import Tracker, TrackerType, TrackerStatus
 
 class TaskForm(forms.Form):
     summary = forms.CharField(max_length=200, required=True, label='Краткое описание')
-    description = forms.CharField(max_length=3000, required=False, label='Полное описание', widget=widgets.Textarea)
+    description = forms.CharField(max_length=3000, required=True, label='Полное описание', widget=widgets.Textarea)
     status = forms.ModelChoiceField(queryset=TrackerStatus.objects.all(), required=True, label='Статус')
     type = forms.ModelMultipleChoiceField(queryset=TrackerType.objects.all(), required=True, label='Тип')
 
