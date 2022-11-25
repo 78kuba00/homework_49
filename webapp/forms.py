e75074e562ecde3c0f8be43c43d56b2359b9a736
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-11-25 13:43:18 +0600

    Made validation for first two fields (summary, description)

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import widgets
+from django.forms import widgets, ValidationError
 from webapp.models import Tracker, TrackerType, TrackerStatus
 
 # class TaskForm(forms.Form):
@@ -16,4 +16,21 @@ class TaskForm(forms.ModelForm):
         model = Tracker
         fields = ['summary', 'description', 'status', 'type']
         widgets = {'status': widgets.CheckboxInput, 'type': widgets.CheckboxSelectMultiple}
-        # type = {'type': widgets.CheckboxSelectMultiple}
+
+    def clean_summary(self):
+        summary = self.cleaned_data['summary']
+        if len(summary) < 5:
+            # raise ValidationError('Краткое описание должно быть не менее %(length)d символов!', code='too_short', params={'length': 5})
+            self.add_error('summary', ValidationError('Краткое описание должно быть не менее %(length)d символов!', code='too_short', params={'length': 5}))
+        return summary
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['summary'] == cleaned_data['description']:
+            raise ValidationError('Текст краткого и полного описания не должны дублировать друг друга')
+    
+    # def clean_description(self):
+    #     description = self.cleaned_data['description']
+    #     if len(description) < 10:
+    #         raise ValidationError('Полное описание слишком короткое! Должно быть не менее %(length)d символов!', code='too_short', params={'length': 10})
+    #     return description
