e6005263e30d864c62551a4fdbb654ecb96ec554
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-12-01 15:58:30 +0600

    trying to add ListView

--- tracker/urls.py
+++ tracker/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import IndexView, TaskView, CreateView, EditView, DeleteView
+from webapp.views import IndexViews, TaskView, CreateView, EditView, DeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
+    path('', IndexViews.as_view(), name='index'),
     path('task/<int:pk>/', TaskView.as_view(), name='task_view'),
     path('task/add/', CreateView.as_view(), name='task_add'),
     path('task/<int:pk>/edit/', EditView.as_view(), name='task_edit'),
-    path('task/<int:pk>/delete', DeleteView.as_view(), name='task_delete')
+    path('task/<int:pk>/delete/', DeleteView.as_view(), name='task_delete')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -4,19 +4,25 @@ from webapp.models import TrackerType, Tracker, TrackerStatus
 from webapp.forms import TaskForm
 
 from django.views.generic import TemplateView, RedirectView, FormView
-from webapp.base_views import FormView as CustomFormView
+from webapp.base_views import FormView as CustomFormView, ListView as CustomListView
 
-class IndexView(TemplateView):
+class IndexViews(CustomListView):
     template_name = 'index.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = Tracker.objects.all()
-        return context
-    def post(self, request, *args, **kwargs):
-        for task_pk in request.POST.getlist('tasks', []):
-            Tracker.objects.get(pk=task_pk).delete()
-        context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
+    model = Tracker
+    context_key = 'tasks'
+
+    def get_objects(self):
+        return Tracker.objects.order_by('-updated_at')
+    # template_name = 'index.html'
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['tasks'] = Tracker.objects.all()
+    #     return context
+    # def post(self, request, *args, **kwargs):
+    #     for task_pk in request.POST.getlist('tasks', []):
+    #         Tracker.objects.get(pk=task_pk).delete()
+    #     context = self.get_context_data(**kwargs)
+    #     return self.render_to_response(context)
 
 class TaskView(TemplateView):
     template_name = 'task_view.html'
@@ -38,22 +44,6 @@ class CreateView(CustomFormView):
     def form_valid(self, form):
         self.task = form.save()
         return super().form_valid(form)
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['form'] = TaskForm()
-    #     return context
-    # def post(self, request, *args, **kwargs):
-    #     form = TaskForm(data=request.POST)
-    #     if form.is_valid():
-    #         types = form.cleaned_data.pop('type')
-    #         task = Tracker.objects.create(**form.cleaned_data)
-    #         task.type.set(types)
-    #         return redirect('task_view', pk=task.pk)
-    #     else:
-    #         context = self.get_context_data(**kwargs)
-    #         context['form'] = form
-    #         return self.render_to_response(context)
 
 class EditView(FormView):
     template_name = "task_edit.html"
@@ -88,35 +78,12 @@ class EditView(FormView):
         pk = self.kwargs.get('pk')
         return get_object_or_404(Tracker, pk=pk)
 
-    #
-    # def get(self, request, *args, **kwargs):
-    #     task = get_object_or_404(Tracker, pk=kwargs['pk'])
-    #     form = TaskForm(initial={
-    #         'summary': task.summary,
-    #         'description': task.description,
-    #         'status': task.status,
-    #         'type': task.type.all()
-    #     })
-    #     return render(request, 'task_edit.html', {'form': form})
-    #
-    # def post (self, request, *args, **kwargs):
-    #     task = get_object_or_404(Tracker, pk=kwargs['pk'])
-    #     form = TaskForm(data=request.POST)
-    #     if form.is_valid():
-    #         task.summary = form.cleaned_data['summary']
-    #         task.description = form.cleaned_data['description']
-    #         task.status = form.cleaned_data['status']
-    #         task.save()
-    #         task.type.set(form.cleaned_data['type'])
-    #         return redirect('task_view', pk=task.pk)
-    #     else:
-    #         return render(request, 'task_edit.html', {'form': form})
 
 class DeleteView(View):
-   def get(self, request, *args, **kwargs):
-       task = get_object_or_404(Tracker, pk=kwargs['pk'])
-       return render(request, 'task_delete.html', {'task': task})
-   def post(self, request, *args, **kwargs):
-       task = get_object_or_404(Tracker, pk=kwargs['pk'])
-       task.delete()
-       return redirect('index')
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Tracker, pk=kwargs['pk'])
+        return render(request, 'task_delete.html', {'task': task})
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Tracker, pk=kwargs['pk'])
+        task.delete()
+        return redirect('index')
