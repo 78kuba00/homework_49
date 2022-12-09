0f3c329915018a44ed0833a8d106da627b8321f8
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-12-09 18:11:57 +0600

    deleted commented code

--- webapp/views/task_views.py
+++ webapp/views/task_views.py
@@ -38,28 +38,6 @@ class TaskCreate(CreateView):
     model = Tracker
     form_class = TaskForm
 
-    # def form_valid(self, form):
-    #     form.instance.project = get_object_or_404(Project, pk=self.kwargs.get('pk'))
-    #     return super().form_valid(form)
-
-    # def get_success_url(self):
-    #     return reverse('project_view', kwargs={'pk': self.object.project.pk})
-
-
-# class TaskEdit(EditView):
-    # def get(self, request, *args, **kwargs):
-    #     task = get_object_or_404(Tracker, pk=kwargs['pk'])
-    #     form = TaskForm(instance=task)
-    #     return render(request, 'task/task_edit.html', {'form': form, 'task': task})
-    #
-    # def post(self, request, *args, **kwargs):
-    #     task = get_object_or_404(Tracker, pk=kwargs['pk'])
-    #     form = TaskForm(instance=task, data=request.POST)
-    #     if form.is_valid():
-    #         task = form.save()
-    #         return redirect('task_view', pk=task.pk)
-    #     else:
-    #         return render(request, 'task/task_edit.html', {'form': form, 'task': task})
 class TaskEdit(EditView):
     form_class = TaskForm
     template_name = 'task/task_edit.html'
@@ -69,34 +47,8 @@ class TaskEdit(EditView):
     redirect_url = 'index'
 
 
-    # def dispatch(self, request, *args, **kwargs):
-    #     self.task = self.get_object()
-    #     return super().dispatch(request, *args, **kwargs)
-
-    # def get_success_url(self):
-    #     return reverse('task/task_view.html', kwargs={'pk': self.object.pk})
-
-    # def get_form_kwargs(self):
-    #     kwargs = super().get_form_kwargs()
-    #     kwargs['instance'] = self.task
-    #     return kwargs
-
-    # def form_valid(self, form):
-    #     self.task = form.save()
-    #     return super().form_valid(form)
-
-    # def get_object(self):
-    #     return get_object_or_404(Tracker, pk=self.kwargs.get('pk'))
-
 class TaskDelete(DeleteView):
     template_name = 'task/task_delete.html'
     model = Tracker
     context_key = 'task'
     redirect_url = reverse_lazy('index')
-    # def get(self, request, *args, **kwargs):
-    #     task = get_object_or_404(Tracker, pk=kwargs['pk'])
-    #     return render(request, 'task/task_delete.html', {'task': task})
-    # def post(self, request, *args, **kwargs):
-    #     task = get_object_or_404(Tracker, pk=kwargs['pk'])
-    #     task.delete()
-    #     return redirect('index')
